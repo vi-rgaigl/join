ef2fdc1131c94bcbfcdd0ef37136cbc4aab2f686
Regina Gaigl <regina.gaigl@gmail.com>
2024-11-09 15:10:36 +0100

    login animation

---
+++ js/login.js
@@ -0,0 +1,5 @@
+function initLogin() {
+    document.getElementById('login-form-box').classList.add('dawn');
+    document.getElementById('login-body').classList.add('dawn');
+    document.getElementById('logo-login').classList.add('logo-position');
+}
