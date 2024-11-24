fc35aa4073ba5fb506172bf1258ba5f134ddf127
Dragon180990 <niko.helwig@stickerei-helwig.de>
2024-11-24 11:52:05 +0100

    added JSDoc

--- js/board.js
+++ js/board.js
@@ -4,6 +4,9 @@ let dragginTaskId;
 let editTask;
 let errorEditTask = { Title: false, DueDate: false };
 
+/**
+ * Inital load of all Data from Firebase
+ */
 async function initLoadData() {
   try {
     tasks = await getData("tasks");
@@ -14,6 +17,9 @@ async function initLoadData() {
   }
 }
 
+/**
+ * Load Tasks from Firebase and render Tasks overview
+ */
 async function loadData() {
   try {
     tasks = await getData("tasks");
@@ -23,6 +29,9 @@ async function loadData() {
   }
 }
 
+/**
+ * Render the Task overview
+ */
 function renderTasks() {
   let rowToDoRef = document.getElementById("to-do");
   let rowInProgessRef = document.getElementById("in-progress");
@@ -38,10 +47,18 @@ function renderTasks() {
   rowDoneRef.innerHTML = getTemplateCards("done", tasks, contacts);
 }
 
+/**
+ * Disable the standart of Drag and Drop
+ * @param {*} ev Drag and Drop event
+ */
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
+/**
+ * Save the id of the dragging Obj and activate the dragging area
+ * @param {string} id - of the dragging task
+ */
 function startDragging(id) {
   document.getElementById(id).classList.add("on-dragging");
   let draggingAreas = document.getElementsByClassName("drag-area");
@@ -50,6 +67,11 @@ function startDragging(id) {
   }
   dragginTaskId = id;
 }
+
+/**
+ * Deaktivate the dragging area
+ * @param {string} id - of the dragging task
+ */
 function endDragging(id) {
   document.getElementById(id).classList.remove("on-dragging");
   let draggingAreas = document.getElementsByClassName("drag-area");
@@ -58,6 +80,10 @@ function endDragging(id) {
   }
 }
 
+/**
+ * Set the status of dragging task to newStatus and load data new.
+ * @param {string} newStatus - New status of the dragging Task
+ */
 async function moveTo(newStatus) {
   let task = tasks.find((task) => task.id === dragginTaskId);
   task.status = newStatus;
@@ -69,28 +95,39 @@ async function moveTo(newStatus) {
   }
 }
 
-function removeHighlight(status) {
-  document
-    .getElementById(status + "-drag-area")
-    .classList.remove("drag-area-active-highlight");
-}
-
+/**
+ * Highlight the status who is hover of dragging Task
+ * @param {string} status - Name of status to highlight
+ */
 function highlight(status) {
   document
     .getElementById(status + "-drag-area")
     .classList.add("drag-area-active-highlight");
 }
 
+/**
+ * Remove highlight of status who is hover of dragging Task
+ * @param {string} status - Name of status to highlight
+ */
+function removeHighlight(status) {
+  document
+    .getElementById(status + "-drag-area")
+    .classList.remove("drag-area-active-highlight");
+}
+
+/**
+ * Open the Dialog with overview of the Task
+ * @param {string} id - of Task
+ */
 function openDialog(id) {
   let boardDialogRef = document.getElementById("boardDialog");
   boardDialogRef.innerHTML = renderDialogTaskOverview(getTask(id), contacts);
   boardDialogRef.showModal();
 }
 
-function closeDialogEdit() {
-  renderOverviewTask(editTask.id);
-}
-
+/**
+ * Close the Dialog with animation
+ */
 function closeDialog() {
   let boardDialogRef = document.getElementById("boardDialog");
   boardDialogRef.classList.add("hide");
@@ -109,12 +146,29 @@ function closeDialog() {
   );
 }
 
+/**
+ * Close the Edit Dialog and rendered the Overview
+ */
+function closeDialogEdit() {
+  renderOverviewTask(editTask.id);
+}
+
+/**
+ * Find the Object of task with id
+ * @param {string} id - of the task
+ * @returns the object of the task
+ */
 function getTask(id) {
   return tasks.find((task) => {
     return task.id === id;
   });
 }
 
+/**
+ * Toggel the status of Subtask and render the Overview new
+ * @param {string} idSubtask - ID of the Subtask
+ * @param {string} idTask - ID od the Task
+ */
 async function subtasktChangeDone(idSubtask, idTask) {
   let currentTask = getTask(idTask);
   currentTask.subtasks[idSubtask].done = !currentTask.subtasks[idSubtask].done;
@@ -128,6 +182,10 @@ async function subtasktChangeDone(idSubtask, idTask) {
   }
 }
 
+/**
+ * Delet the Task and render the Overview new
+ * @param {string} idTask - ID of Task
+ */
 async function deleteTask(idTask) {
   let currentTask = getTask(idTask);
   try {
@@ -139,22 +197,37 @@ async function deleteTask(idTask) {
   }
 }
 
+/**
+ * Render the edit dialog of task
+ * @param {string} id - ID of Task
+ */
 function renderEditTask(id) {
   let boardDialogRef = document.getElementById("boardDialog");
   editTask = structuredClone(getTask(id));
   boardDialogRef.innerHTML = renderDialogTaskEdit(editTask, contacts);
 }
 
+/**
+ * Render the overview dialog of task
+ * @param {string} id - ID of Task
+ */
 function renderOverviewTask(id) {
   let boardDialogRef = document.getElementById("boardDialog");
   boardDialogRef.innerHTML = renderDialogTaskOverview(getTask(id), contacts);
 }
 
+/**
+ * Toggel the status of dropdown
+ */
 function toggleDropdown() {
   let dropdownAssignedRef = document.getElementById("dropdownAssinged");
   dropdownAssignedRef.classList.toggle("dropdown-open");
 }
 
+/**
+ * validate the title and change in the task
+ * @param {event} event - input event
+ */
 function changeTitle(event) {
   let title = event.target.value;
   if (title.length > 0) {
@@ -166,10 +239,18 @@ function changeTitle(event) {
   editTask.title = title;
 }
 
+/**
+ * Change the description in the task
+ * @param {event} event - input event
+ */
 function changeDescription(event) {
   editTask.description = event.target.value;
 }
 
+/**
+ * validate the due date and change in the task
+ * @param {event} event - input event
+ */
 function changeDueDate(event) {
   let dueDate = event.target.value;
   if (dueDate.length <= 0) {
@@ -183,6 +264,11 @@ function changeDueDate(event) {
   editTask.dueDate = dueDate;
 }
 
+/**
+ * Set the error to the input field
+ * @param {string} text - Error massage to render
+ * @param {string} field - Field to set the error
+ */
 function errorMassage(text, field) {
   let errorRef = document.getElementById(`error${field}`);
   let inputRef = document.getElementById(`input${field}`);
@@ -197,6 +283,11 @@ function errorMassage(text, field) {
   }
 }
 
+/**
+ * Check if the Date is past
+ * @param {string} date - in format YYYY-MM-DD
+ * @returns true if the date is past
+ */
 function checkIfPast(date) {
   let dateTask = new Date(date + ":00:00.000");
   let dateNow = new Date();
@@ -204,10 +295,18 @@ function checkIfPast(date) {
   return dateTask < dateNow;
 }
 
+/**
+ * Change the prio in the task
+ * @param {event} event - input event
+ */
 function changePrio(event) {
   editTask.prio = event.target.value;
 }
 
+/**
+ * Change the assignedTo in the task
+ * @param {event} event - input event
+ */
 function changeAssignedTo(event) {
   let assignedUsersRefs = document.querySelectorAll(
     "input[type=checkbox][name=assignedUsers]"
@@ -223,6 +322,10 @@ function changeAssignedTo(event) {
   renderAssignedTo(editTask);
 }
 
+/**
+ * rendered the Names of assigned contacs
+ * @param {object} task - set the assigned Names
+ */
 function renderAssignedTo(task) {
   let assingedUsersRef = document.getElementById("assingedUsers");
   assingedUsersRef.innerHTML = getAssignInitialsNameEdit(
@@ -231,6 +334,10 @@ function renderAssignedTo(task) {
   );
 }
 
+/**
+ * Toggel the visibility of delete Subtask in edit task
+ * @param {event} event - input event
+ */
 function changeVisibilityButton(event) {
   let subtaskCheckBtnRef = document.getElementById("subtaskCheckBtn");
   if (event.target.value.length > 0) {
@@ -240,6 +347,9 @@ function changeVisibilityButton(event) {
   }
 }
 
+/**
+ * Added a new Subtask and render the list of Subtasks new
+ */
 function addNewSubtask() {
   let inputSubtasksRef = document.getElementById("inputSubtasks");
   let subtaskCheckBtnRef = document.getElementById("subtaskCheckBtn");
@@ -255,16 +365,27 @@ function addNewSubtask() {
   renderSubtasks(editTask);
 }
 
+/**
+ * Rendered the Subtasks of task
+ * @param {object} task - Object to render
+ */
 function renderSubtasks(task) {
   let listSubtasksRef = document.getElementById("listSubtasks");
   listSubtasksRef.innerHTML = getListOfSubtasksEdit(task.subtasks);
 }
 
+/**
+ * Delet the Subtask and render list of Subtask new
+ * @param {number} indexOfSubtak - index of Subtask
+ */
 function deleteSubtask(indexOfSubtak) {
   editTask.subtasks.splice(indexOfSubtak, 1);
   renderSubtasks(editTask);
 }
 
+/**
+ * Check if a error of Input and disable the button submit
+ */
 function checkIfError() {
   let btnSubmitEditTaskRef = document.getElementById("btnSubmitEditTask");
   let isError = false;
@@ -276,6 +397,10 @@ function checkIfError() {
   btnSubmitEditTaskRef.disabled = isError;
 }
 
+/**
+ * change the the task with the edits and load the Data new. Render task overview
+ * @param {string} id - ID of task
+ */
 async function submitEditTask(id) {
   try {
     await changeData("tasks", editTask);
