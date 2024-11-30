c3d14d88e879a43e2bd8da27e52acf2967fb5940
Niko Helwig <niko.helwig@stickerei-helwig.de>
2024-11-30 22:11:07 +0100

    Revert "data varibles deleted"
    This reverts commit bb95eee184c41890ca8e7006b75dc7f889900902.

--- templates/boardDialog.template.js
+++ templates/boardDialog.template.js
@@ -92,12 +92,12 @@ function renderDialogTaskEdit(task, contacts) {
               </div>
               <div class="input-row">
                 <label class="lable">Assinged to</label>
-                <div class="input dropdown select-arrow" id="dropdownAssinged" >
-                  <div class="dropdown-click" onclick="toggleDropdown()" >
+                <div class="input dropdown select-arrow" id="dropdownAssinged" data-dropdown=true>
+                  <div class="dropdown-click" onclick="toggleDropdown()" data-dropdown=true>
                     Select contacts to assign
                   </div>
-                  <div class="dropdown-list" >
-                    <hr class="dropdown-hr" />
+                  <div class="dropdown-list" data-dropdown=true>
+                    <hr class="dropdown-hr" data-dropdown=true/>
                     ${getListOfUsers(task.assignedTo, contacts)}
                   </div>
                 </div>
@@ -297,9 +297,9 @@ function getListOfSubtasksEdit(subtasks) {
 function getListOfUsers(assignedTo, contacts) {
   let html = "";
   contacts.forEach((contact) => {
-    html += `<div class="dropdown-list-item" >
+    html += `<div class="dropdown-list-item" data-dropdown=true>
               <input
-                
+                data-dropdown=true
                 type="checkbox"
                 id="user${contact.id}"
                 name="assignedUsers"
@@ -307,7 +307,9 @@ function getListOfUsers(assignedTo, contacts) {
                 onchange="changeAssignedTo(event)"
                 ${ifUserAssigned(assignedTo, contact.id)}
                 />
-              <label for="user${contact.id}" >${contact.name}</label>
+              <label for="user${contact.id}" data-dropdown=true>${
+      contact.name
+    }</label>
             </div>`;
   });
 
