aef32de3257b663015c60b47510e2f30573419fe
Niko Helwig <niko.helwig@stickerei-helwig.de>
2024-12-03 21:20:22 +0100

    responsive contact name

--- templates/responsiveView.js
+++ templates/responsiveView.js
@@ -1,30 +1,34 @@
 function toggleResponsiveView(mode) {
-    let contactListContainer = document.getElementById("contactListContainer");
-    let contactDetailsContainer = document.getElementById("contactDetailsContainer");
-    let backBtn = document.querySelector(".back-btn");
+  let contactListContainer = document.getElementById("contactListContainer");
+  let contactDetailsContainer = document.getElementById(
+    "contactDetailsContainer"
+  );
+  let backBtn = document.querySelector(".back-btn");
 
-    if (mode === "details" && window.innerWidth <= 635) {
-        contactListContainer.style.display = "none";
-        contactDetailsContainer.style.display = "block";
+  if (mode === "details" && window.innerWidth <= 635) {
+    contactListContainer.style.display = "none";
+    contactDetailsContainer.style.display = "block";
 
-        if (backBtn) {
-            backBtn.classList.add("active");
-        }
-    } else {
-        contactListContainer.style.display = "block";
-        contactDetailsContainer.style.display = "block";
-
-        if (backBtn) {
-            backBtn.classList.remove("active");
-        }
+    if (backBtn) {
+      backBtn.classList.add("active");
     }
+  } else {
+    contactListContainer.style.display = "block";
+    contactDetailsContainer.style.display = "none";
+
+    if (backBtn) {
+      backBtn.classList.remove("active");
+    }
+  }
 }
 
 function backToContactList() {
-    let contactListContainer = document.getElementById("contactListContainer");
-    let contactDetailsContainer = document.getElementById("contactDetailsContainer");
+  let contactListContainer = document.getElementById("contactListContainer");
+  let contactDetailsContainer = document.getElementById(
+    "contactDetailsContainer"
+  );
 
-    contactListContainer.style.display = "block";
-    contactDetailsContainer.style.display = "none";
-    toggleResponsiveView("list");
+  contactListContainer.style.display = "block";
+  contactDetailsContainer.style.display = "none";
+  toggleResponsiveView("list");
 }
