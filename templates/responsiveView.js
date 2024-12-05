90690d44224eb11c12e083254329b494967a9ff6
Niko Helwig <niko.helwig@stickerei-helwig.de>
2024-12-05 19:39:28 +0100

    code formating

--- templates/responsiveView.js
+++ templates/responsiveView.js
@@ -1,24 +1,17 @@
 function toggleResponsiveView(mode) {
+  let backBtn = document.querySelector(".back-btn");
   let contactListContainer = document.getElementById("contactListContainer");
   let contactDetailsContainer = document.getElementById(
     "contactDetailsContainer"
   );
-  let backBtn = document.querySelector(".back-btn");
-
   if (mode === "details" && window.innerWidth <= 635) {
     contactListContainer.style.display = "none";
     contactDetailsContainer.style.display = "block";
-
-    if (backBtn) {
-      backBtn.classList.add("active");
-    }
+    if (backBtn) backBtn.classList.add("active");
   } else if (window.innerWidth <= 635) {
     contactListContainer.style.display = "block";
     contactDetailsContainer.style.display = "none";
-
-    if (backBtn) {
-      backBtn.classList.remove("active");
-    }
+    if (backBtn) backBtn.classList.remove("active");
   }
 }
 
