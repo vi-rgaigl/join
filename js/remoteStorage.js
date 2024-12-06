e8dcc4250ae1e3ef18d446e90c9293db376f5acc
NIKHEL <niko.helwig@stickerei-helwig.de>
2024-12-06 11:45:54 +0100

    dont deleted Id

--- js/remoteStorage.js
+++ js/remoteStorage.js
@@ -66,7 +66,7 @@ async function pushData(endpoint, data) {
  */
 async function changeData(endpoint, data) {
   let url = `${Base_URL}${endpoint}/${data.id}.json`;
-  delete data.id;
+  // delete data.id;
   try {
     await fetch(url, {
       method: "PUT",
