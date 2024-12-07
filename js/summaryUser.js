5e82dc9ab9cca59cb21458f2c08cf8ba6f499174
Regina Gaigl <regina.gaigl@gmail.com>
2024-12-07 13:44:58 +0100

    update  jsdoc

--- js/summaryUser.js
+++ js/summaryUser.js
@@ -1,5 +1,9 @@
 let tasks;
 let user;
+
+/**
+ * Loads the summary data, including tasks and user information, and renders the summary.
+ */
 async function loadSummary() {
   try {
     tasks = await getData("tasks");
@@ -11,16 +15,25 @@ async function loadSummary() {
   }
 }
 
+/**
+ * Renders the summary data into the element with the ID 'summary-data'.
+ */
 function renderSummary() {
   let summaryRef = document.getElementById("summary-data");
   summaryRef.innerHTML = getTemplateSummary(tasks, user);
 }
 
+/**
+ * Loads user data from local storage.
+ */
 function loadDataFromLocalStorage() {
   let respons = getFromLocalStorage("join393");
   user = respons.user;
 }
 
+/**
+ * Renders the mobile greeting into the element with the ID 'summary-greacing-mobile'.
+ */
 function renderMobileGreasing() {
   let greasingRef = document.getElementById("summary-greacing-mobile");
   greasingRef.innerHTML = getGreacing(user);
